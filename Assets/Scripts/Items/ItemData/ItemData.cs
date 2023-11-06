97c3a0b7afaf3b7b6393e3356af829cb50ca3733
Cris-Marie <cmvn2003@gmail.com>
2023-11-05 22:03:15 -0300

    Inventário

--- Assets/Scripts/Items/ItemData/ItemData.cs
+++ Assets/Scripts/Items/ItemData/ItemData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "My Assets/Item Data")]
 public class ItemData : ScriptableObject
--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -22,41 +23,105 @@ public class Inventory : MonoBehaviour
         }
     }
 
-    public List<ItemData> inventoryItems = new List<ItemData>();
+    public List<GameObject> smallInventoryItems = new List<GameObject>();
+    public Transform itemContent;   //gameobject/transform da posição que o slot vai fazer instantiate
+    public Image inventoryItem; //prefab do slot
+
+    public List<GameObject> bigInventoryItem = new List<GameObject>();
+    public Transform inventoryItemBig;
+    public Image bigInventory;
+
+    public bool ItemP = false, ItemG = false;
+
+    private Interactable interagivel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        interagivel = FindObjectOfType<Interactable>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Teste para ver se remove da lista e do inventário da UI... /funciona
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U) || bigInventoryItem.Count > 1)
+        {
+            GameObject itemClone = Instantiate(bigInventoryItem[0], gameObject.transform.position, Quaternion.identity);
+            itemClone.SetActive(true);
+            bigInventoryItem.Remove(bigInventoryItem[0]);            
+        }
     }
 
-    public void AddItem(ItemData item)
+    public void AddItem(GameObject item)
     {
-        inventoryItems.Add(item);
+        //inventoryItems.Add(item);
+
+        ListItems(item);
     }
 
-    public void RemoveItem(ItemData item)
+    public void RemoveItem(GameObject item)
     {
-        inventoryItems.Remove(item);
+        smallInventoryItems.Remove(item);
+    }
+
+    public void ListItems(GameObject item)
+    {
+        if (ItemP)
+        {
+            smallInventoryItems.Add(item);
+
+            foreach (var ItemData in smallInventoryItems)
+            {
+                Image img = Instantiate(inventoryItem, itemContent);
+                var itemIcon = img.transform.GetComponent<Image>();
+
+                itemIcon.sprite = item.GetComponent<ItemData>().sprite;
+            }
+        }
+        else if (ItemG)
+        {
+            bigInventoryItem.Add(item);
+            Image img = Instantiate(bigInventory, inventoryItemBig);
+
+            img.sprite = item.GetComponent<ItemData>().sprite;   
+        }       
     }
 
     public bool HasItem(int id)
     {
         // Iterate through the items in the inventory
-        foreach (ItemData item in inventoryItems)
+        foreach (GameObject item in smallInventoryItems)
         {
             // Check if the item's itemID matches the specified itemID
-            if (item.id == id)
+            if (item.GetComponent<ItemData>().id == id)
             {
                 return true; // The item was found in the inventory
             }
         }
 
         return false; // The item was not found in the inventory
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("ItemPequeno"))
+        {
+            ItemP = true;
+            ItemG = false;
+        }
+        else if (other.CompareTag("ItemGrande"))
+        {           
+            ItemP = false;
+            ItemG = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ItemPequeno") || other.CompareTag("ItemGrande"))
+        {
+            ItemP = false;
+            ItemG = false;
+        }
     }
 }
