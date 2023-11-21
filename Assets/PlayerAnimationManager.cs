94e94e30ded39f72029a1c585d8533b92821d20c
WVI11IM <wvi11im97@gmail.com>
2023-11-21 14:36:41 -0300

    Animation fixes

---
+++ Assets/PlayerAnimationManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationManager : MonoBehaviour
+{
+    Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    public void Teleport(Transform position)
+    {
+        gameObject.transform.position = position.transform.position;
+        gameObject.transform.rotation = position.transform.rotation;
+    }
+
+    public void isSitting(bool isActive)
+    {
+        playerAnimator.SetBool("isSitting", isActive);
+    }
+}
